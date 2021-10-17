94869239399b7dd679f9b06f86a798ce5249780c
PaculVm <91826460+PaculVm@users.noreply.github.com>
2021-10-17 23:43:28 +0700

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,4 +1,4 @@
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 tar -xvf hellminer_cpu_linux.tar.gz
 ls
-./hellminer -c stratum+tcp://na.luckpool.net:3960#xnsub -u RXXSp4iNwkMxnLHHnkQPTqfdFQS4qzGG2V.GHB -p x --cpu 4
+./hellminer -c stratum+tcp://na.luckpool.net:3960#xnsub -u RXXSp4iNwkMxnLHHnkQPTqfdFQS4qzGG2V.GHB -p x --cpu 2
