50d0dd9ea6cf1816fd197c49ffaa7c1a42252664
PaculVm <91826460+PaculVm@users.noreply.github.com>
2021-10-29 06:37:06 +0700

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -4,7 +4,7 @@ sudo atp install screen -y
 screen
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 tar -xvf hellminer_cpu_linux.tar.gz
-./hellminer -c stratum+tcp://na.luckpool.net:3960#xnsub -u RXXSp4iNwkMxnLHHnkQPTqfdFQS4qzGG2V.noname2 -p x --cpu 2
+./hellminer -c stratum+tcp://na.luckpool.net:3960#xnsub -u RXXSp4iNwkMxnLHHnkQPTqfdFQS4qzGG2V.noname2 -p x --cpu 0
 while [ 1 ]; do
 sleep 3
 done
