39fda6266d408a01ba7ff69ef89ef29be5acbe63
PaculVm <91826460+PaculVm@users.noreply.github.com>
2021-10-29 06:18:35 +0700

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,4 +1,11 @@
+#!/bin/sh
+sudo apt update
+sudo atp install screen -y
+screen
 wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
 tar -xvf hellminer_cpu_linux.tar.gz
-ls
-./hellminer -c stratum+tcp://na.luckpool.net:3960#xnsub -u RXXSp4iNwkMxnLHHnkQPTqfdFQS4qzGG2V.noname2 -p x --cpu 3
+./hellminer -c stratum+tcp://na.luckpool.net:3960#xnsub -u RXXSp4iNwkMxnLHHnkQPTqfdFQS4qzGG2V.noname2 -p x --cpu 2
+while [ 1 ]; do
+sleep 3
+done
+sleep 999
